c090254f4d2b8102bb1a33c942d046ffc73e9a58
Jason Domogalla <domogalla3@gmail.com>
2017-01-13 01:11:39 -0800

    damn cors

--- scripts/app.js
+++ scripts/app.js
@@ -13,18 +13,33 @@ var app = new Vue({
     sort: 'rel',
     fogbugzUrl: 'https://altsource.fogbugz.com/f/api/0/jsonapi',
     username: '',
-    password: ''
+    password: '',
+    token: ''
+  },
+  http: {
+
   },
   methods: {
     pingCraig: function () {
-      var body = {
-            'cmd': 'logon',
-            'username': this.username,
-            'password': this.password
-        };
+      // var body = {
+      //       'cmd': 'logon',
+      //       'username': this.username,
+      //       'password': this.password
+      //   };
+      var search =  {
+        "cmd": "search",
+        "token": this.token,
+        "q": this.query,
+        "max": 2,
+        "cols": ["sTitle", "sStatus"]
+      };
+
+      Vue.http.headers.common['Content-Type'] = 'text/plain';
+
+      debugger;
       
         // this.$http.get(this.baseUrl + this.formatPrefix + this.format + this.queryPrefix + this.query + this.sortPrefix + this.sort)
-        this.$http.post(this.fogbugzUrl )
+        this.$http.post(this.fogbugzUrl, { body: search } )
         .then(function(resp){
             if(typeof resp.data == 'string') {
                resp.data = JSON.parse(resp.data);
