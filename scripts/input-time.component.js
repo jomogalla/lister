09a9b9082484594fbad267ae08d1a3364d2ab959
jomogalla <domogalla3@gmail.com>
2018-01-19 08:00:37 -0800

    Updated Time editing to use more Moment

--- scripts/input-time.component.js
+++ scripts/input-time.component.js
@@ -13,7 +13,7 @@ Vue.component('input-time', {
 	computed: {
 		displayValue: function () {
 			if(this.value) {
-				return moment(this.value).format('hh:mma');	
+				return moment(this.value).format('h:mma');	
 			} else {
 				return "";
 			}
@@ -21,29 +21,36 @@ Vue.component('input-time', {
 	},
 	methods: {
 		updateTimePicked: function (newValue) {
-			var timeSplit = newValue.match(/[a-zA-Z]+|[0-9]+/g);
+			// var timeSplit = newValue.match(/[a-zA-Z]+|[0-9]+/g);
 
-			var twentyFourHourOffset = timeSplit[2] === 'pm' ? 12 : 0;
-			// Turn 12am into 00
-			if(timeSplit[2] === 'am' && timeSplit[0] == 12) {
-				var hour = 0;
-			} else {
-				var hour = parseInt(timeSplit[0]) + twentyFourHourOffset;
-			}
+			// var twentyFourHourOffset = timeSplit[2] === 'pm' ? 12 : 0;
+			// // Turn 12am into 00
+			// if(timeSplit[2] === 'am' && timeSplit[0] == 12) {
+			// 	var hour = 0;
+			// } else {
+			// 	var hour = parseInt(timeSplit[0]) + twentyFourHourOffset;
+			// }
 
 			
-			var minute = parseInt(timeSplit[1]);
+			// var minute = parseInt(timeSplit[1]);
 
-			if(hour > 24 || minute > 59) { return; }
+			// if(hour > 24 || minute > 59) { return; }
 	
-			var formattedValue = moment(this.value);
-			formattedValue.set({'hour': hour, 'minute': minute});
+			// var formattedValue = moment(this.value);
+			// formattedValue.set({'hour': hour, 'minute': minute});
+			var timeFormat = 'h:mma';
+			var formattedValue = moment(newValue, timeFormat);
+			var currentValue = moment(this.value);
+
+			formattedValue.set('year', currentValue.year());
+			formattedValue.set('month', currentValue.month());
+			formattedValue.set('date', currentValue.date());
 
 			// If the value was not already normalized,
 			// manually override it to conform
-			if (formattedValue.format('hh:mma') !== newValue) {
-				this.$refs.input.value = formattedValue.toISOString()
-			}
+			// if (formattedValue.format('hh:mma') !== newValue) {
+			// 	this.$refs.input.value = formattedValue.toISOString()
+			// }
 
 			// Emit the number value through the input event
 			this.$emit('input', String(formattedValue.toISOString()))
